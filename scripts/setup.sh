50f9a1ae1e870f51981f134200277a66582ed0b7
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-06-23 13:10:12 -0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,19 +11,6 @@ echo 'export ACM_ROOT_DIR=/home/aci-user/cloned-repos/agile-cloud-manager/' >> /
 echo 'export ACM_ROOT_DIR=/home/aci-user/cloned-repos/agile-cloud-manager/' >> /etc/bashrc
 echo 'export ACM_ROOT_DIR=/home/aci-user/cloned-repos/agile-cloud-manager/' >> /etc/profile
 
-#Install Python 3 (VM may already have Python 2 installed, which is not high enough for our needs.)
-## Install Python 3.7 (Note: Next time try different location outside of PATH to see if it is added automatically to PATH.)
-#Install Python 3.7
-yum install -y gcc openssl-devel bzip2-devel libffi-devel
-cd /usr/local/bin/
-wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz
-tar xzf Python-3.7.7.tgz
-cd Python-3.7.7
-./configure --enable-optimizations
-make altinstall
-rm /usr/local/bin/Python-3.7.7.tgz
-python3.7 -V
-
 ##################################################
 ## Install Terraform
 cd /home/aci-user
@@ -44,4 +31,3 @@ wget https://github.com/microsoft/terraform-provider-azuredevops/releases/downlo
 tar xzf terraform-provider-azuredevops_linux_amd64.tar.gz
 rm terraform-provider-azuredevops_linux_amd64.tar.gz
 cd /home/aci-user/cloned-repos/agile-cloud-manager/calls-to-modules/azure-pipelines-project-repo-build-resources/
-
