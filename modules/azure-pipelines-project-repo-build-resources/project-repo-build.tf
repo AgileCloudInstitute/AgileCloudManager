7fdc48bc68e0692db9446cd80e89d0ea46278ea9
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-06-25 15:08:19 -0700

    Update project-repo-build.tf

--- modules/azure-pipelines-project-repo-build-resources/project-repo-build.tf
+++ modules/azure-pipelines-project-repo-build-resources/project-repo-build.tf
@@ -11,7 +11,8 @@ resource "azuredevops_git_repository" "repository" {
   project_id = azuredevops_project.project.id
   name = "my-simple-repo"
   provisioner "local-exec" {
-    command = "ls -al && echo $AZ_PAT && echo ${azuredevops_git_repository.repository.ssh_url}"
+    #command = "ls -al && echo $AZ_PAT && echo ${azuredevops_git_repository.repository.ssh_url}"
+    command = "ls -al && echo ${azuredevops_git_repository.repository.name} && echo ${azuredevops_project.project.project_name}"
     working_dir = "/home/aci-user/cloned-repos/agile-cloud-manager/pipeline-scipts/" 
   }  
 }
@@ -19,7 +20,6 @@ resource "azuredevops_git_repository" "repository" {
 resource "azuredevops_build_definition" "build" {
   project_id = azuredevops_project.project.id
   name       = "Sample Build Definition"
-  
   ci_trigger {
     use_yaml = true
   }
@@ -31,5 +31,4 @@ resource "azuredevops_build_definition" "build" {
     #branch_name = azuredevops_git_repository.repository.default_branch
     yml_path    = "azure-pipelines.yml"
   }
-
 }
