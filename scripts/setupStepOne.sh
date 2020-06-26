73d1dc652dc63b8a6b7e1c042d89b8688afa3267
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-06-26 13:12:42 -0700

    Update setupStepOne.sh

--- scripts/setupStepOne.sh
+++ scripts/setupStepOne.sh
@@ -34,3 +34,26 @@ mv terraform /usr/local/bin/
 # tar xzf terraform-provider-azuredevops_linux_amd64.tar.gz
 # rm terraform-provider-azuredevops_linux_amd64.tar.gz
 # cd /home/aci-user/cloned-repos/agile-cloud-manager/calls-to-modules/azure-pipelines-project-repo-build-resources/
+
+###################################################  
+## Install the azure-cli  
+  
+#Get the microsoft key 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+#Set the local yum pointer to the official microsoft repo for azure-cli
+sudo sh -c 'echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+
+#Workaround a bug in the az-cli installation that fails to see that Python 3.6+ is already installed.
+#Identify the most recent version of the RPM from this address: https://packages.microsoft.com/yumrepos/azure-cli/
+#Then specify the RPM explicitly in the following line:
+sudo rpm -ivh --nodeps azure-cli-2.8.0-1.el7.x86_64.rpm
+
+#Install the azure-devops extension of the az-cli
+az extension add --name azure-devops
+
