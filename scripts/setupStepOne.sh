4dcb3acf0cae268aa64082d592a5608587d02230
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-06-26 14:10:24 -0700

    Update setupStepOne.sh

--- scripts/setupStepOne.sh
+++ scripts/setupStepOne.sh
@@ -51,7 +51,8 @@ gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azu
 
 #Workaround a bug in the az-cli installation that fails to see that Python 3.6+ is already installed.
 #Identify the most recent version of the RPM from this address: https://packages.microsoft.com/yumrepos/azure-cli/
-#Then specify the RPM explicitly in the following line:
+#Then specify the RPM explicitly in the following lines:
+wget https://packages.microsoft.com/yumrepos/azure-cli/azure-cli-2.8.0-1.el7.x86_64.rpm
 sudo rpm -ivh --nodeps azure-cli-2.8.0-1.el7.x86_64.rpm
 
 #Install the azure-devops extension of the az-cli
