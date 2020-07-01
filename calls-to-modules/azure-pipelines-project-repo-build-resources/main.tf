b3fd514750184cfa57474fccf53a39dc603ae60b
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-01 15:38:30 -0700

    Update main.tf

--- calls-to-modules/azure-pipelines-project-repo-build-resources/main.tf
+++ calls-to-modules/azure-pipelines-project-repo-build-resources/main.tf
@@ -37,18 +37,18 @@ variable "repoName" { default = "terraform-aws-simple-example" }
 variable "buildName" { default = "terraform-aws-simple-example" }
 variable "awsPublicAccessKey" { }
 variable "awsSecretAccessKey" { }
-
-##Output variables
-output "azuredevops_project_id" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_project_id}" }
-output "azuredevops_git_repository_id" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_id}" }
-output "azuredevops_git_repository_name" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_name}" }
-output "azuredevops_git_repository_default_branch" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_default_branch}" }
-output "azuredevops_git_repository_is_fork" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_is_fork}" }
-output "azuredevops_git_repository_remote_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_remote_url}" }
-output "azuredevops_git_repository_size" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_size}" }
-output "azuredevops_git_repository_ssh_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_ssh_url}" }
-output "azuredevops_git_repository_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_url}" }
-output "azuredevops_git_repository_web_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_web_url}" }
-output "azuredevops_project_name" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_project_name}" }
-
   
+##Output variables  
+output "azuredevops_project_id" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_project_id}" }  
+output "azuredevops_git_repository_id" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_id}" }  
+output "azuredevops_git_repository_name" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_name}" }  
+output "azuredevops_git_repository_default_branch" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_default_branch}" }  
+output "azuredevops_git_repository_is_fork" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_is_fork}" }  
+output "azuredevops_git_repository_remote_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_remote_url}" }  
+output "azuredevops_git_repository_size" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_size}" }  
+output "azuredevops_git_repository_ssh_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_ssh_url}" }  
+output "azuredevops_git_repository_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_url}" }  
+output "azuredevops_git_repository_web_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_git_repository_web_url}" }  
+output "azuredevops_project_name" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_project_name}" }  
+output "azuredevops_build_definition_id" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_build_definition_id" }  
+output "azuredevops_organization_service_url" { value = "${module.azure-pipelines-project-repo-build-resources.azuredevops_organization_service_url" }  
