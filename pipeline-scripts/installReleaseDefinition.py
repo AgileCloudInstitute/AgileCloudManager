e0b56e129937cf9f026047d7a7dbd466ad848bc6
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-09 13:01:15 -0700

    Update installReleaseDefinition.py

--- pipeline-scripts/installReleaseDefinition.py
+++ pipeline-scripts/installReleaseDefinition.py
@@ -1,6 +1,11 @@
 print("Inside installReleaseDefinition.py script.")  
   
 import sys  
+import requests
+import os
+import base64
+import json
+
 import deploymentFunctions as depfunc  
   
 pathToAzdoProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azdo-provider.tfvars'
@@ -19,7 +24,7 @@ pathToProjectRepoBuildCalls = "/home/aci-user/cloned-repos/agile-cloud-manager/c
 outputProjectRepoBuildCommand='terraform output '
 
 ##############################################################################################
-### Step One: Get The Outpur Variables From the azure-pipelines-project-repo-build module
+### Step One: Get The Output Variables From the azure-pipelines-project-repo-build module
 ##############################################################################################
 
 depfunc.runTerraformCommand(initCommand, pathToProjectRepoBuildCalls)
@@ -33,3 +38,26 @@ print("depfunc.azuredevops_project_id is: ", depfunc.azuredevops_project_id )
 print("depfunc.azuredevops_project_name is: ", depfunc.azuredevops_project_name)
 print("depfunc.azuredevops_organization_service_url is: ", depfunc.azuredevops_organization_service_url)
 print("azuredevops_key_vault_name is: ", depfunc.azuredevops_key_vault_name)
+print("azuredevops_organization_name is: ", depfunc.azuredevops_organization_name)
+
+##############################################################################################
+### Step Two: Get The queueId from the agent pool that will be used by the release definition.
+##############################################################################################
+def getApiRequest(url):
+    personal_access_token = ":"+os.environ["AZ_PAT"]
+    headers = {}
+    headers['Content-type'] = "application/json"
+    headers['Authorization'] = b'Basic ' + base64.b64encode(personal_access_token.encode('utf-8'))
+    r = requests.get(url, headers=headers)
+    print("r.status_code is: ", r.status_code)
+    print("r.json() is: ", r.json())
+
+
+#Get a list of Agent Pool Queues
+api_version_p = "5.1-preview.1"
+queue_name = "Default"
+#GET https://dev.azure.com/{organization}/{project}/_apis/distributedtask/queues?queueName={queueName}&actionFilter={actionFilter}&api-version=5.1-preview.1
+queues_url = ("https://dev.azure.com/%s/%s/_apis/distributedtask/queues?queueName=%s&api-version=%s" % (azuredevops_organization_name, azuredevops_project_id, queue_name, api_version_p))
+print("-------------------------------------------------------------")
+print("---- About to get list of Agent Pool Job Queues ----")
+getApiRequest(queues_url)
