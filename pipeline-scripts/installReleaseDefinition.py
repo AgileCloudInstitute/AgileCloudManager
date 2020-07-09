38af8ab3862d66aa4692eed9ac48b18d2ba92e8e
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-09 10:28:24 -0700

    Create installReleaseDefinition.py

---
+++ pipeline-scripts/installReleaseDefinition.py
@@ -0,0 +1,33 @@
+print("Inside installReleaseDefinition.py script.")  
+  
+import sys  
+import deploymentFunctions as depfunc  
+  
+pathToAzdoProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azdo-provider.tfvars'
+pathToAzurermProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azurerm-provider.tfvars'
+pathToAzdoProjectRepoBuildAutoInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-auto.tfvars'
+pathToAzdoProjectRepoBuildManualInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-manual.tfvars'
+
+getAzdoProviderInputs=' -var-file='+pathToAzdoProviderInputs
+getAzurermProviderInputs=' -var-file='+pathToAzurermProviderInputs
+getAzdoProjectRepoBuildAutoInputs=' -var-file='+pathToAzdoProjectRepoBuildAutoInputs
+getAzdoProjectRepoBuildManualInputs=' -var-file='+pathToAzdoProjectRepoBuildManualInputs
+
+initCommand='terraform init'
+outputCommand='terraform output '
+
+pathToProjectRepoBuildCalls = "/home/aci-user/cloned-repos/agile-cloud-manager/calls-to-modules/azure-pipelines-project-repo-build-resources/"
+outputProjectRepoBuildCommand=applyCommand+getAzdoProviderInputs+getAzdoProjectRepoBuildAutoInputs+getAzdoProjectRepoBuildManualInputs+getAzurermProviderInputs
+
+##############################################################################################
+### Step Three: Apply The azure-pipelines-project-repo-build module
+##############################################################################################
+
+depfunc.runTerraformCommand(initCommand, pathToProjectRepoBuildCalls)
+depfunc.runTerraformCommand(outputProjectRepoBuildCommand, pathToProjectRepoBuildCalls)
+
+print("Back in installReleaseDefinition.py .")
+
+print("depfunc.azuredevops_project_name is: ", depfunc.azuredevops_project_name)
+print("depfunc.azuredevops_git_repository_name is: ", depfunc.azuredevops_git_repository_name)
+
