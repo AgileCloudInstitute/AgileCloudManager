8bc9456e7c70c12749fc584857151f7a1a9b923f
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-09 14:28:57 -0700

    Update installReleaseDefinition.py

--- pipeline-scripts/installReleaseDefinition.py
+++ pipeline-scripts/installReleaseDefinition.py
@@ -64,3 +64,51 @@ queuesData = getApiRequest(queues_url)
 print("---------------------------------------------------------")
 #Using index 0 here because queue_name should be a unique key that brings only one result in this response
 print("queueId is: ", queuesData['value'][0]['id'])  
+
+
+##############################################################################################
+### Step Three: Create Release Definition By Making API Call.
+##############################################################################################
+def createReleaseDefinitionApiRequest(templateFile, azdo_organization_name, azdo_project_id):
+    personal_access_token = ":"+os.environ["AZ_PAT"]
+    headers = {}
+    headers['Content-type'] = "application/json"
+    headers['Authorization'] = b'Basic ' + base64.b64encode(personal_access_token.encode('utf-8'))
+    api_version = "5.1"
+    url = ("https://vsrm.dev.azure.com/%s/%s/_apis/release/definitions?api-version=%s" % (azuredevops_organization_name, azuredevops_project_id, api_version))
+    with open(templateFile, 'r') as json_file:
+      data = json.load(json_file)
+      print("---------------------------------------------------------")
+      print("name is: ", data['name'])
+      data['name'] = 'Create AWS Simple Example'
+      print("name is now: ", data['name'])
+      print("---------------------------------------------------------")
+      print("[\'artifacts\'][\'sourceId\'] is: ", data['artifacts'][0]['sourceId'])
+      print("[\'artifacts\'][\'artifactSourceDefinitionUrl\'][\'id\'] is: ", data['artifacts'][0]['artifactSourceDefinitionUrl']['id'])
+      print("[\'artifacts\'][\'alias\'] is: ", data['artifacts'][0]['alias'])
+      print("[\'artifacts\'][\'definitionReference\'][\'definition\'][\'id\'] is: ", data['artifacts'][0]['definitionReference']['definition']['id'])
+      print("[\'artifacts\'][\'definitionReference\'][\'definition\'][\'name\'] is: ", data['artifacts'][0]['definitionReference']['definition']['name'])
+      print("[\'artifacts\'][\'definitionReference\'][\'project\'][\'id\'] is: ", data['artifacts'][0]['definitionReference']['project']['id'])
+      print("[\'artifacts\'][\'definitionReference\'][\'project\'][\'name\'] is: ", data['artifacts'][0]['definitionReference']['project']['name'])
+      print("---------------------------------------------------------")
+      data['artifacts'][0]['sourceId'] = azuredevops_project_id + ":1"
+      data['artifacts'][0]['artifactSourceDefinitionUrl']['id'] = azuredevops_organization_service_url + azuredevops_project_name + "/_build?definitionId=" + str(azuredevops_build_definition_id)
+      data['artifacts'][0]['alias'] = "_" + azuredevops_git_repository_name
+      data['artifacts'][0]['definitionReference']['definition']['id'] = azuredevops_build_definition_id
+      data['artifacts'][0]['definitionReference']['definition']['name'] = azuredevops_git_repository_name
+      data['artifacts'][0]['definitionReference']['project']['id'] = azuredevops_project_id
+      data['artifacts'][0]['definitionReference']['project']['name'] = azuredevops_project_name
+      print("---------------------------------------------------------")
+      print("[\'artifacts\'][\'sourceId\'] is: ", data['artifacts'][0]['sourceId'])
+      print("[\'artifacts\'][\'artifactSourceDefinitionUrl\'][\'id\'] is: ", data['artifacts'][0]['artifactSourceDefinitionUrl']['id'])
+      print("[\'artifacts\'][\'alias\'] is: ", data['artifacts'][0]['alias'])
+      print("[\'artifacts\'][\'definitionReference\'][\'definition\'][\'id\'] is: ", data['artifacts'][0]['definitionReference']['definition']['id'])
+      print("[\'artifacts\'][\'definitionReference\'][\'definition\'][\'name\'] is: ", data['artifacts'][0]['definitionReference']['definition']['name'])
+      print("[\'artifacts\'][\'definitionReference\'][\'project\'][\'id\'] is: ", data['artifacts'][0]['definitionReference']['project']['id'])
+      print("[\'artifacts\'][\'definitionReference\'][\'project\'][\'name\'] is: ", data['artifacts'][0]['definitionReference']['project']['name'])
+      print("---------------------------------------------------------")
+      print("url is: ", url)
+      print("---------------------------------------------------------")
+      print("revised data is: ", data)
+
+createReleaseDefinitionApiRequest('releaseDefinitionTemplate.json', depfunc.azuredevops_organization_name, depfunc.azuredevops_project_id)
