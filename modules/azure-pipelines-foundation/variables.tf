b2908adccb0b66d6ce01c07664cf5d6cc94f5742
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-23 13:12:55 -0700

    Update variables.tf

--- modules/azure-pipelines-foundation/variables.tf
+++ modules/azure-pipelines-foundation/variables.tf
@@ -11,7 +11,6 @@ variable "tenantId" { }
 #The client referred to is an App Registration.  MAKE A FINAL DECISION ABOUT WHAT THIS WILL BE HERE AND DOCUMENT IT IN VISIO.
 variable "clientId" { }
 variable "clientSecret" { }
-variable "storageAccountNameTerraformBackend" { }
 variable "pipeAzureRegion" { }
 
 # Workstation External IP. Override with variable or hardcoded value if necessary.
@@ -21,7 +20,7 @@ locals { admin-external-cidr = "${chomp(data.http.admin-external-ip.body)}/32" }
 ##Define the output variables
 output "pipes_resource_group_name" { value = azurerm_resource_group.pipelines.name }
 output "pipes_resource_group_region" { value = azurerm_resource_group.pipelines.location }
-output "pipes_storage_account_name" { value = azurerm_storage_account.terraformBknd.name }
+output "pipes_subnet_id" { value = azurerm_subnet.pipelines.id }
 output "nicName" { value = azurerm_network_interface.myterraformnic.name }
 output "storageAccountDiagName" { value = azurerm_storage_account.mystorageaccount.name }
 output "pipeKeyVaultName" { value = azurerm_key_vault.infraPipes.name }
