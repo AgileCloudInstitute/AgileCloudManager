d2e9fb25c6eb81ea306bcf05296474f96b0192e2
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-23 14:46:05 -0700

    Update key-vault.tf

--- modules/azure-pipelines-project-repo-build-resources/key-vault.tf
+++ modules/azure-pipelines-project-repo-build-resources/key-vault.tf
@@ -1,11 +1,9 @@
-
-
+  
 data "azurerm_key_vault" "pipeKeyVault" {
   name                = var.pipeKeyVaultName
   resource_group_name = var.pipeResourceGroupName
 }
-
-
+  
 resource "azurerm_key_vault_access_policy" "azdo-project-access-key-vault" {
   key_vault_id = data.azurerm_key_vault.pipeKeyVault.id
   #Test of a second access_policy.  The following block contains the ID of the AzureDevops project with which this release is associated, so this release can access the key vault.  
@@ -36,16 +34,16 @@ resource "azurerm_key_vault_secret" "awsSecretAccessKey" {
 resource "azurerm_key_vault_secret" "storageAccountNameTerraformBackend" {
   name         = "storageAccountNameTerraformBackend"
   value        = var.storageAccountNameTerraformBackend
-  key_vault_id = azurerm_key_vault.infraPipes.id
+  key_vault_id = data.azurerm_key_vault.pipeKeyVault.id
   tags = { environment = "Testing" }
-  depends_on = [azurerm_key_vault_access_policy.userCreatedSP]
+  depends_on = [azurerm_key_vault_access_policy.azdo-project-access-key-vault]
 }
 
 resource "azurerm_key_vault_secret" "terraBackendKey" {
   name         = "terra-backend-key"
   value        = azurerm_storage_account.terraformBknd.primary_access_key
-  key_vault_id = azurerm_key_vault.infraPipes.id
+  key_vault_id = data.azurerm_key_vault.pipeKeyVault.id
   tags = { environment = "Testing" }
-  depends_on = [azurerm_key_vault_access_policy.userCreatedSP]
+  depends_on = [azurerm_key_vault_access_policy.azdo-project-access-key-vault]
 }
 
