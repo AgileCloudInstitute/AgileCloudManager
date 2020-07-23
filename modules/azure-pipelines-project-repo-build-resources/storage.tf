0e64bfcd60c99e1737df6c587567425a3d4fcaaf
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-23 13:04:33 -0700

    Update storage.tf

--- modules/azure-pipelines-project-repo-build-resources/storage.tf
+++ modules/azure-pipelines-project-repo-build-resources/storage.tf
@@ -1,14 +1,30 @@
 #The terraform backend will be located in this storage container, which will be placed in the imported storage account.
 
-data "azurerm_storage_account" "terraform-backend" {
+resource "azurerm_storage_account" "terraformBknd" {
   name                = var.storageAccountNameTerraformBackend
   resource_group_name = var.pipeResourceGroupName
+
+  location                 = ??
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    #CHANGE THE FOLLOWING TO "Deny" FOR PRODUCTION.
+    default_action             = "Allow"
+    #ip_rules                   = [var.myRemoteIP, azurerm_public_ip.myterraformpublicip.ip_address]
+    virtual_network_subnet_ids = [??]
+  }
+
+  tags = {
+    environment = "dev"
+  }
 }
+
 
 ######Each Terraform Backend will require its own storage container as follows:
 resource "azurerm_storage_container" "terraformBknd" {
   name                  = var.storageContainerNameTerraformBackend
-  storage_account_name  = data.azurerm_storage_account.terraform-backend.name
+  storage_account_name  = azurerm_storage_account.terraformBknd.name
   #Lock down the following line when this is ready to be made more secure.  See this link: https://docs.microsoft.com/en-us/azure/storage/blobs/storage-manage-access-to-resources?tabs=dotnet
   container_access_type = "container"
 }
