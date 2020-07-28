de08ae6a3cd8c6bab2eed0c1c3c699dcc138d853
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-28 15:19:39 -0700

    Update installReleaseDefMinimal.py

--- pipeline-scripts/installReleaseDefMinimal.py
+++ pipeline-scripts/installReleaseDefMinimal.py
@@ -6,19 +6,8 @@ import os
 import base64
 import json
 import deploymentFunctions as depfunc  
-  
-pathToAzdoProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azdo-provider.tfvars'
-pathToAzurermProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azurerm-provider.tfvars'
-pathToAzdoProjectRepoBuildAutoInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-auto.tfvars'
-pathToAzdoProjectRepoBuildManualInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-manual.tfvars'
-
-getAzdoProviderInputs=' -var-file='+pathToAzdoProviderInputs
-getAzurermProviderInputs=' -var-file='+pathToAzurermProviderInputs
-getAzdoProjectRepoBuildAutoInputs=' -var-file='+pathToAzdoProjectRepoBuildAutoInputs
-getAzdoProjectRepoBuildManualInputs=' -var-file='+pathToAzdoProjectRepoBuildManualInputs
 
 initCommand='terraform init'
-
 pathToProjectRepoBuildCalls = "/home/aci-user/cloned-repos/agile-cloud-manager/calls-to-modules/azure-pipelines-project-repo-build-resources/"
 outputProjectRepoBuildCommand='terraform output '
 
@@ -29,34 +18,46 @@ outputProjectRepoBuildCommand='terraform output '
 depfunc.runTerraformCommand(initCommand, pathToProjectRepoBuildCalls)
 depfunc.runTerraformCommand(outputProjectRepoBuildCommand, pathToProjectRepoBuildCalls)
 
-print("Back in installReleaseDefinition.py .")
-print("depfunc.azuredevops_build_definition_id is: ", depfunc.azuredevops_build_definition_id)
-print("depfunc.azuredevops_git_repository_id is: ", depfunc.azuredevops_git_repository_id)
-print("depfunc.azuredevops_git_repository_name is: ", depfunc.azuredevops_git_repository_name)
+print("Back in installReleaseDefMinimal.py .")
 print("depfunc.azuredevops_project_id is: ", depfunc.azuredevops_project_id )
-print("depfunc.azuredevops_project_name is: ", depfunc.azuredevops_project_name)
-print("depfunc.azuredevops_organization_service_url is: ", depfunc.azuredevops_organization_service_url)
-
-print("depfunc.azuredevops_key_vault_name is: ", depfunc.azuredevops_key_vault_name)
 print("depfunc.azuredevops_organization_name is: ", depfunc.azuredevops_organization_name)
-
-print("depfunc.azuredevops_service_connection_id is: ", depfunc.azuredevops_service_connection_id)
 
 #########################################################################################################
 ### Step Two: Get The poolQueueId from the agent pool Queue that will be used by the release definition.
-#########################################################################################################
-queue_name = "Default"
-poolQueueId = depfunc.getPoolQueueIdApiRequest(depfunc.azuredevops_organization_name, depfunc.azuredevops_project_id, queue_name)
-print("poolQueueId is: ", poolQueueId)  
-print("---------------------------------------------------------")
-
+################################################################################templateFile, azdo_organization_name, azdo_project_id, releaseDefName, releaseDefDescription#########################
+def createReleaseDefinitionApiRequest(templateFile, azdo_organization_name, azdo_project_id, releaseDefName, releaseDefDescription):
+    personal_access_token = ":"+os.environ["AZ_PAT"]
+    headers = {}
+    headers['Content-type'] = "application/json"
+    headers['Authorization'] = b'Basic ' + base64.b64encode(personal_access_token.encode('utf-8'))
+    api_version = "5.1"
+    url = ("https://vsrm.dev.azure.com/%s/%s/_apis/release/definitions?api-version=%s" % (azdo_organization_name, azdo_project_id, api_version))
+    with open(templateFile, 'r') as json_file:
+      print("json_file is: ", json_file)
+      data = json.load(json_file)
+      print("---------------------------------------------------------")
+      print("name is: ", data['name'])
+      data['name'] = releaseDefName
+      print("name is now: ", data['name'])
+      print("description is: ", data['description'])
+      data['description'] = releaseDefDescription
+      print("description is now: ", data['description'])
+      print("---------------------------------------------------------")
+      print("url is: ", url)
+      print("---------------------------------------------------------")
+      print("revised data is: ", data)
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    respCode = r.status_code
+    print("r.status_code is: ", respCode)
+    print("r.json() is: ", r.json())
+    return respCode
+ 
 ##############################################################################################
 ### Step Three: Create Release Definition By Making API Call.
 ##############################################################################################
-artifactAlias = "_" + depfunc.azuredevops_git_repository_name
-jsonTemplateFile = 'releaseDefinitionTemplate.json'
-myScriptInputVars = "$(-aws-public-access-key)  $(-aws-secret-access-key)  $(storageAccountNameTerraformBackend)  $(terra-backend-key)  $(aws-region)  $(System.DefaultWorkingDirectory)"
-releaseDefinitionName = 'Create AWS Simple Example'
-environmentName = 'Name of environment from user-supplied script'
-rCode = depfunc.createReleaseDefinitionApiRequest(jsonTemplateFile, depfunc.azuredevops_organization_name, depfunc.azuredevops_project_id, depfunc.azuredevops_project_name, depfunc.azuredevops_build_definition_id, depfunc.azuredevops_git_repository_name, depfunc.azuredevops_organization_service_url, poolQueueId, artifactAlias, depfunc.azuredevops_service_connection_id, myScriptInputVars, releaseDefinitionName, environmentName)
+jsonTemplateFile = 'releaseDefTempMinimal.json'
+releaseDefinitionName = 'Name of minimal release Definition'
+releaseDefinitionDescription = 'Description of minimal release definition.'
+rCode = createReleaseDefinitionApiRequest(jsonTemplateFile, depfunc.azuredevops_organization_name, depfunc.azuredevops_project_id , releaseDefinitionName, releaseDefinitionDescription)
+
 print("response code from create release definition API call is: ", rCode)
