a2e51548870e4645963b4cda53c59987866346d2
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-07-28 16:58:29 -0700

    Update constructReleaseDef.py

--- pipeline-scripts/constructReleaseDef.py
+++ pipeline-scripts/constructReleaseDef.py
@@ -3,3 +3,12 @@ import json
 data = json.load(open('pythonTaskTemplate.json', 'r'))
 
 print("data is: ", data)
+
+data['name'] = 'new name'
+data['inputs']['scriptPath'] = 'new path'
+data['inputs']['arguments'] = 'new arguments'
+data['inputs']['workingDirectory'] = 'new working directory'
+
+print("revised data is: ", data)
+
+
