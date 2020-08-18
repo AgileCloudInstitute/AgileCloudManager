d0f1c5ed9cef5f8e810124f695af51afb8892ab8
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-18 16:15:54 -0700

    Update deploymentFunctions.py

--- pipeline-tasks/deploymentFunctions.py
+++ pipeline-tasks/deploymentFunctions.py
@@ -365,6 +365,57 @@ def setEnvironmentVars(yamlInputFile, setEnvironmentVarsFile):
           line = line.replace("export AZURE_DEVOPS_EXT_PAT=","export AZURE_DEVOPS_EXT_PAT="+azdoOrgPAT)
     print('{}'.format(line))
 
+def getFoundationInputs(yamlInputFile, foundationSecretsFile):
+  varsString = ''
+  with open(yamlInputFile) as f:
+    topLevel_dict = yaml.safe_load(f)
+    for item in topLevel_dict:
+      print("item is: ", item)
+      if re.match("meta", item):
+        metaItems = topLevel_dict.get(item)
+        for metaItem in metaItems: 
+          if re.match("subscriptionId", metaItem):
+            print(metaItem, " is: ", metaItems.get(metaItem))
+            varsString = varsString + " -var=\""+ metaItem + "=" + metaItems.get(metaItem) +"\""  
+          if re.match("tenantId", metaItem):
+            print(metaItem, " is: ", metaItems.get(metaItem))
+            varsString = varsString + " -var=\""+ metaItem + "=" + metaItems.get(metaItem) +"\""  
+          if re.match("pipeAzureRegion", metaItem):
+            print(metaItem, " is: ", metaItems.get(metaItem))
+            varsString = varsString + " -var=\""+ metaItem + "=" + metaItems.get(metaItem) +"\""  
+      if re.match("connection", item):  
+        connectionItems = topLevel_dict.get(item)  
+        for connectionItem in connectionItems:
+          if re.match("clientId", connectionItem):
+            print(connectionItem, " is: ", connectionItems.get(connectionItem))
+            varsString = varsString + " -var=\""+ connectionItem + "=" + connectionItems.get(connectionItem) +"\""  
+          if re.match("clientSecret", connectionItem):
+            print(connectionItem, " is: ", connectionItems.get(connectionItem))
+            with open(foundationSecretsFile, "w") as file:
+              lineToAdd = connectionItem+"="+connectionItems.get(connectionItem)
+              file.write(lineToAdd)
+              varsString = varsString + " -var-file=\""+ foundationSecretsFile +"\""  
+      if re.match("importProjectRepoBuild", item):
+        projectRepoBuildCollections = topLevel_dict.get(item)
+        for projectRepoBuild in projectRepoBuildCollections:
+          for projectRepoBuildItem in projectRepoBuild: 
+            if re.match("s3BucketNameTF", projectRepoBuildItem):
+              print(projectRepoBuildItem, " is: ", projectRepoBuild.get(projectRepoBuildItem))
+              varsString = varsString + " -var=\""+ projectRepoBuildItem + "=" + projectRepoBuild.get(projectRepoBuildItem) +"\""  
+            if re.match("dynamoDbTableNameTF", projectRepoBuildItem):
+              print(projectRepoBuildItem, " is: ", projectRepoBuild.get(projectRepoBuildItem))
+              varsString = varsString + " -var=\""+ projectRepoBuildItem + "=" + projectRepoBuild.get(projectRepoBuildItem) +"\""  
+            if re.match("moduleKeys", projectRepoBuildItem):
+              print("-----------------------------------------------")
+              print("-----------------------------------------------")
+              moduleKeysCollection = projectRepoBuild.get(projectRepoBuildItem)
+              for moduleKey in moduleKeysCollection:
+                if re.match(moduleKey.get("name"), "s3KeyNameTF_Foundation"):
+                  print(moduleKey.get("name"), " ::: has key ::: ", moduleKey.get("value"))
+                  varsString = varsString + " -var=\""+ moduleKey.get("name") + "=" + moduleKey.get("value") +"\""  
+  print("varsString is: ", varsString)
+  return varsString
+
 ### End of the new stuff
 #####################################################################################################################################
 #####################################################################################################################################
