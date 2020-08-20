3f67f5c66305768de59b5e1d83305b0f35851cae
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-19 17:12:06 -0700

    Update terraform.tf

--- calls-to-modules/azure-pipelines-foundation-demo/terraform.tf
+++ calls-to-modules/azure-pipelines-foundation-demo/terraform.tf
@@ -3,12 +3,12 @@ terraform {
   backend "s3" {}
 }
 
-data "terraform_remote_state" "state" {
-  backend = "s3"
-  config {
-    bucket         = var.s3BucketNameTF
-    dynamodb_table = var.dynamoDbTableNameTF
-    region         = var.pipeAzureRegion
-    key            = var.s3KeyNameTF
-  }
-}
+#data "terraform_remote_state" "state" {
+#  backend = "s3"
+#  config {
+#    bucket         = var.s3BucketNameTF
+#    dynamodb_table = var.dynamoDbTableNameTF
+#    region         = var.pipeAzureRegion
+#    key            = var.s3KeyNameTF
+#  }
+#}
