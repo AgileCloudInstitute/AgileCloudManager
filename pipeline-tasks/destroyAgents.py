cdf0487b84a7d9e4d791f23883f4c36fb7d97ded
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-24 14:02:55 -0700

    Update destroyAgents.py

--- pipeline-tasks/destroyAgents.py
+++ pipeline-tasks/destroyAgents.py
@@ -4,72 +4,52 @@ import os
 import sys 
 import deploymentFunctions as depfunc
 
-# #The following are created/populated when you run  setup.sh at the time you download this repo.
-# pathToAgentAutoInputs='/home/aci-user/vars/agile-cloud-manager/inputs-agent-vms-auto.tfvars'
-# pathToAgentManualInputs='/home/aci-user/vars/agile-cloud-manager/inputs-agent-vms-manual.tfvars'
-# pathToAzdoProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azdo-provider.tfvars'
-# pathToAzurermProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azurerm-provider.tfvars'
-# pathToFoundationInputs='/home/aci-user/vars/agile-cloud-manager/inputs-foundation-demo.tfvars'
-# pathToAzdoProjectRepoBuildInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build.tfvars'
-
-# getAgentAutoInputs=' -var-file='+pathToAgentAutoInputs
-# getAgentManualInputs=' -var-file='+pathToAgentManualInputs
-# getAzdoProviderInputs=' -var-file='+pathToAzdoProviderInputs
-# getAzurermProviderInputs=' -var-file='+pathToAzurermProviderInputs
-# getFoundationInputs=' -var-file='+pathToFoundationInputs
-# getAzdoProjectRepoBuildInputs=' -var-file='+pathToAzdoProjectRepoBuildInputs
-
 myYamlInputFile = '/home/aci-user/staging/enter-user-input-here-only.yaml'
 foundationSecretsFile = '/home/aci-user/vars/agile-cloud-manager/foundation-secrets.tfvars'
 
 #The awsCredFile is for the terraform backend that will store state for the azure infrastructure created for the agile cloud manager.
 awsCredFile = '/home/aci-user/.aws/credentials'
-
 initCommand='terraform init '
+backendFoundationConfig = depfunc.getFoundationBackendConfig(myYamlInputFile, awsCredFile)
+initBackendFoundationCommand = initCommand + backendFoundationConfig
 
-#////////////////
-backendConfig = depfunc.getFoundationBackendConfig(myYamlInputFile, awsCredFile)
-initBackendCommand = initCommand + backendConfig
-
-destroyCommand='terraform destroy -auto-approve'
-foundationVars = depfunc.getFoundationInputs(myYamlInputFile, foundationSecretsFile)
-destroyFoundationCommand=destroyCommand+foundationVars
-
+outputCommand = 'terraform output '
 #Environment variable set during cloud-init instantiation
 acmRootDir=os.environ['ACM_ROOT_DIR']
 pathToFoundationCalls = acmRootDir+"calls-to-modules/azure-pipelines-foundation-demo/"
+  
+##############################################################################################
+### Step One: Get Output from The azure-pipelines-foundation-demo module
+##############################################################################################
+depfunc.runTerraformCommand(initBackendFoundationCommand, pathToFoundationCalls)
+depfunc.runTerraformCommand(outputCommand, pathToFoundationCalls)
 
-print ('foundationVars is: :', foundationVars )
+print("depfunc.subscription_id  is: ", depfunc.subscription_id)
+print("depfunc.tenant_id  is: ", depfunc.tenant_id)
+print("depfunc.resourceGroupLocation  is: ", depfunc.resourceGroupLocation)
+print("depfunc.resourceGroupName  is: ", depfunc.resourceGroupName)
+print("depfunc.nicName  is: ", depfunc.nicName)
+print("depfunc.storName is: ", depfunc.storName)
 
 ##############################################################################################
-### Step One: Apply The azure-pipelines-foundation-demo module
+### Step Two: Prepare the input variables for the azure-pipelines-agent-vms-demo module
 ##############################################################################################
-# depfunc.runTerraformCommand(initBackendCommand, pathToFoundationCalls)
-# depfunc.runTerraformCommand(applyFoundationCommand, pathToFoundationCalls)
-#////////////////
-
-# destroyCommand='terraform destroy -auto-approve'
-# destroyFoundationCommand=destroyCommand+getAzurermProviderInputs+getFoundationInputs
-
-# #Environment variable set during cloud-init instantiation
-# acmRootDir=os.environ['ACM_ROOT_DIR']
-# pathToFoundationCalls = acmRootDir+"calls-to-modules/azure-pipelines-foundation-demo/"
-# pathToAgentCalls = acmRootDir+"/calls-to-modules/azure-pipelines-agent-vms-demo/"
-# destroyAgentCommand=destroyCommand+getAzurermProviderInputs+getAgentAutoInputs+getAgentManualInputs
-
-# print ('pathToFoundationInputs:', pathToFoundationInputs )
-# print ('pathToAgentAutoInputs:', pathToAgentAutoInputs )
-
-# ##############################################################################################
-# ### Step One: Destroy the azure-pipelines-agent-vms-demo module
-# ##############################################################################################
-# depfunc.runTerraformCommand(initCommand, pathToAgentCalls)
-# depfunc.runTerraformCommand(destroyAgentCommand, pathToAgentCalls)
-# print("Back in destroyPipelineSystem.py .")
+destroyCommand='terraform destroy -auto-approve'
+agentsVars = depfunc.getAgentsInputs(myYamlInputFile, foundationSecretsFile, depfunc.subscription_id, depfunc.tenant_id, depfunc.resourceGroupLocation, depfunc.resourceGroupName, depfunc.nicName, depfunc.storName )
+destroyAgentsCommand=destroyCommand+agentsVars
+pathToAgentCalls = acmRootDir+"calls-to-modules/azure-pipelines-agent-vms-demo/"
+print ('agentsVars is: :', agentsVars )
 
 ##############################################################################################
-### Step Two: Destroy the azure-pipelines-foundation-demo module
+### Step Three: Prepare the backend config for the azure-pipelines-agent-vms-demo module
 ##############################################################################################
-depfunc.runTerraformCommand(initBackendCommand, pathToFoundationCalls)
-depfunc.runTerraformCommand(destroyFoundationCommand, pathToFoundationCalls)
-print("Back in destroyPipelineSystem.py .")
+backendAgentsConfig = depfunc.getAgentsBackendConfig(myYamlInputFile, awsCredFile)
+initAgentsCommand = initCommand + backendAgentsConfig
+print("backendAgentsConfig is: ", backendAgentsConfig)
+
+##############################################################################################
+### Step Four: Initialize the Terraform backend for the azure-pipelines-agent-vms-demo module
+##############################################################################################
+depfunc.runTerraformCommand(initAgentsCommand, pathToAgentCalls)
+depfunc.runTerraformCommand(destroyAgentsCommand, pathToAgentCalls)
+print("Back in installPipelineSystem.py .")
