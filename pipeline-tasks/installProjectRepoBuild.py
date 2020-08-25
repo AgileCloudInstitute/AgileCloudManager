119f332847f6b9404518f39605a8af4dd4a92bcd
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-24 17:15:46 -0700

    Update installProjectRepoBuild.py

--- pipeline-tasks/installProjectRepoBuild.py
+++ pipeline-tasks/installProjectRepoBuild.py
@@ -3,22 +3,6 @@ print("Inside installProjectRepoBuild.py script.")
 import sys 
 import deploymentFunctions as depfunc
 
-pathToAzdoProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azdo-provider.tfvars'
-pathToAzurermProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azurerm-provider.tfvars'
-pathToAzdoProjectRepoBuildAutoInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-auto.tfvars'
-pathToAzdoProjectRepoBuildManualInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-manual.tfvars'
-
-getAzdoProviderInputs=' -var-file='+pathToAzdoProviderInputs
-getAzurermProviderInputs=' -var-file='+pathToAzurermProviderInputs
-getAzdoProjectRepoBuildAutoInputs=' -var-file='+pathToAzdoProjectRepoBuildAutoInputs
-getAzdoProjectRepoBuildManualInputs=' -var-file='+pathToAzdoProjectRepoBuildManualInputs
-
-initCommand='terraform init'
-applyCommand='terraform apply -auto-approve'
-
-pathToProjectRepoBuildCalls = "/home/aci-user/cloned-repos/agile-cloud-manager/calls-to-modules/azure-pipelines-project-repo-build-resources/"
-applyProjectRepoBuildCommand=applyCommand+getAzdoProviderInputs+getAzdoProjectRepoBuildAutoInputs+getAzdoProjectRepoBuildManualInputs+getAzurermProviderInputs
-
 ##############################################################################################
 ### Step One:  Install azure devops extension for az client
 ##############################################################################################
@@ -44,6 +28,59 @@ def runShellCommand(commandToRun):
 addExteensionCommand = 'az extension add --name azure-devops'
 runShellCommand(addExteensionCommand)
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+myYamlInputFile = '/home/aci-user/staging/enter-user-input-here-only.yaml'
+foundationSecretsFile = '/home/aci-user/vars/agile-cloud-manager/foundation-secrets.tfvars'
+
+#The awsCredFile is for the terraform backend that will store state for the azure infrastructure created for the agile cloud manager.
+awsCredFile = '/home/aci-user/.aws/credentials'
+initCommand='terraform init '
+backendFoundationConfig = depfunc.getFoundationBackendConfig(myYamlInputFile, awsCredFile)
+initBackendFoundationCommand = initCommand + backendFoundationConfig
+
+outputCommand = 'terraform output '
+#Environment variable set during cloud-init instantiation
+acmRootDir=os.environ['ACM_ROOT_DIR']
+pathToFoundationCalls = acmRootDir+"calls-to-modules/azure-pipelines-foundation-demo/"
+  
+##############################################################################################
+### Step One: Get Output from The azure-pipelines-foundation-demo module
+##############################################################################################
+depfunc.runTerraformCommand(initBackendFoundationCommand, pathToFoundationCalls)
+depfunc.runTerraformCommand(outputCommand, pathToFoundationCalls)
+
+print("depfunc.subscription_id  is: ", depfunc.subscription_id)
+print("depfunc.tenant_id  is: ", depfunc.tenant_id)
+print("depfunc.resourceGroupLocation  is: ", depfunc.resourceGroupLocation)
+print("depfunc.resourceGroupName  is: ", depfunc.resourceGroupName)
+print("depfunc.nicName  is: ", depfunc.nicName)
+print("depfunc.storName is: ", depfunc.storName)
+
+##############################################################################################
+### Step Two: Prepare the input variables for the azure-pipelines-project-repo-build-resources module
+##############################################################################################
+applyCommand='terraform apply -auto-approve'
+prbVars = depfunc.getAgentsInputs(myYamlInputFile, foundationSecretsFile, depfunc.subscription_id, depfunc.tenant_id, depfunc.resourceGroupLocation, depfunc.resourceGroupName, depfunc.nicName, depfunc.storName )
+applyPrbCommand=applyCommand+prbVars
+pathToPrbCalls = acmRootDir+"calls-to-modules/azure-pipelines-project-repo-build-resources/"
+print ('prbVars is: :', prbVars )
+  
+################################################################################################ 
+### Step Three: Prepare the backend config for the azure-pipelines-project-repo-build-resources module
+##############################################################################################
+backendPrbConfig = depfunc.getAgentsBackendConfig(myYamlInputFile, awsCredFile)
+initPrbCommand = initCommand + backendPrbConfig
+print("backendPrbConfig is: ", backendPrbConfig)
+
+##############################################################################################
+### Step Five: Initialize the Terraform backend for the azure-pipelines-project-repo-build-resources module
+##############################################################################################
+depfunc.runTerraformCommand(initPrbCommand, pathToPrbCalls)
+depfunc.runTerraformCommand(applyPrbCommand, pathToPrbCalls)
+print("Back in installPipelineSystem.py .")
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ##############################################################################################
 ### Step Two: Apply The azure-pipelines-project-repo-build module
