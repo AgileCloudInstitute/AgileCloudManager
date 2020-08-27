56e2c97a48965fe01e212d11604e494f6b211456
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-27 16:37:20 -0700

    Update deploymentFunctions.py

--- pipeline-tasks/deploymentFunctions.py
+++ pipeline-tasks/deploymentFunctions.py
@@ -681,6 +681,20 @@ def getProjectsReposBuildInputs(yamlInputFile, awsCredFile, prbSecretsFile, subs
   print("varsString is: ", varsString)
   return varsString
 
+def runShellCommand(commandToRun):
+    print("Inside runShellCommand(..., ...) function. ")
+    print("commandToRun is: " +commandToRun)
+
+    proc = subprocess.Popen( commandToRun,cwd=None, stdout=subprocess.PIPE, shell=True)
+    while True:
+      line = proc.stdout.readline()
+      if line:
+        thetext=line.decode('utf-8').rstrip('\r|\n')
+        decodedline=ansi_escape.sub('', thetext)
+        print(decodedline)
+      else:
+        break
+  
 ### End of the new stuff
 #####################################################################################################################################
 #####################################################################################################################################
