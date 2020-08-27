92f4fb62a0c9cbd2e24dfc9249dca26f01729c7d
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-27 16:52:48 -0700

    Update destroyProjectRepoBuild.py

--- pipeline-tasks/destroyProjectRepoBuild.py
+++ pipeline-tasks/destroyProjectRepoBuild.py
@@ -1,36 +1,75 @@
 print("Inside destroyProjectRepoBuild.py script.")  
-  
-import sys  
-import deploymentFunctions as depfunc  
-  
-pathToAzdoProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azdo-provider.tfvars'
-pathToAzurermProviderInputs='/home/aci-user/vars/agile-cloud-manager/inputs-azurerm-provider.tfvars'
-pathToAzdoProjectRepoBuildAutoInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-auto.tfvars'
-pathToAzdoProjectRepoBuildManualInputs='/home/aci-user/vars/agile-cloud-manager/inputs-project-repo-build-manual.tfvars'
-  
-getAzdoProviderInputs=' -var-file='+pathToAzdoProviderInputs
-getAzurermProviderInputs=' -var-file='+pathToAzurermProviderInputs
-getAzdoProjectRepoBuildAutoInputs=' -var-file='+pathToAzdoProjectRepoBuildAutoInputs
-getAzdoProjectRepoBuildManualInputs=' -var-file='+pathToAzdoProjectRepoBuildManualInputs
+import sys 
+import deploymentFunctions as depfunc
+import os 
+import yaml 
+    
+##############################################################################################
+### Step One:  Install azure devops extension for az client
+##############################################################################################
+addExteensionCommand = 'az extension add --name azure-devops'
+depfunc.runShellCommand(addExteensionCommand)
 
-initCommand='terraform init'
+myYamlInputFile = '/home/aci-user/staging/projectRepoBuildConfig.yaml'
+foundationSecretsFile = '/home/aci-user/vars/agile-cloud-manager/foundation-secrets.tfvars'
+prbSecretsFile = '/home/aci-user/vars/agile-cloud-manager/prb-secrets.tfvars'
+
+#The awsCredFile is for the terraform backend that will store state for the azure infrastructure created for the agile cloud manager.
+awsCredFile = '/home/aci-user/.aws/credentials'
+initCommand='terraform init '
+backendFoundationConfig = depfunc.getFoundationBackendConfig(myYamlInputFile, awsCredFile)
+initBackendFoundationCommand = initCommand + backendFoundationConfig
+
+outputCommand = 'terraform output '
+#Environment variable set during cloud-init instantiation
+acmRootDir=os.environ['ACM_ROOT_DIR']
+pathToFoundationCalls = acmRootDir+"calls-to-modules/azure-pipelines-foundation-demo/"
+  
+##############################################################################################
+### Step Two: Get Output from The azure-pipelines-foundation-demo module
+##############################################################################################
+depfunc.runTerraformCommand(initBackendFoundationCommand, pathToFoundationCalls)
+depfunc.runTerraformCommand(outputCommand, pathToFoundationCalls)
+
+print("depfunc.subscription_id  is: ", depfunc.subscription_id)
+print("depfunc.tenant_id  is: ", depfunc.tenant_id)
+print("depfunc.resourceGroupLocation  is: ", depfunc.resourceGroupLocation)
+print("depfunc.resourceGroupName  is: ", depfunc.resourceGroupName)
+print("depfunc.pipeKeyVaultName is: ", depfunc.pipeKeyVaultName)
+print("depfunc.pipeSubnetId is: ", depfunc.pipeSubnetId)
+print("depfunc.azuredevops_subscription_name is: ", depfunc.azuredevops_subscription_name)
+print("depfunc.subscription_name is: ", depfunc.subscription_name)
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+# HOW SHOULD SECRETS FILES BE MANAGED?  foundationSecretsFile  awsCredFile
+# HOW SHOULD MULTIPLE PROJECTREPOBUILD DEFINITIONS BE MANAGED?
+# HOW SHOULD MULTIPLE BACKENDS BE MANAGED?  
+# STILL NEED TO CREATE: depfunc.getProjectRepoBuildInputs(...)  AND depfunc.getProjectRepoBuildBackendConfig(...)
+#//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+##############################################################################################
+### Step Three: Prepare the input variables for the azure-pipelines-project-repo-build-resources module
+##############################################################################################
+
+prbBackendConfig = depfunc.getProjectRepoBuildBackendConfig(myYamlInputFile, awsCredFile)
+prbInputs = depfunc.getProjectsReposBuildInputs(myYamlInputFile, awsCredFile, prbSecretsFile, depfunc.subscription_id, depfunc.tenant_id, depfunc.resourceGroupLocation, depfunc.resourceGroupName, depfunc.pipeKeyVaultName, depfunc.pipeSubnetId, depfunc.subscription_name )
+
+print("prbBackendConfig is: ", prbBackendConfig)
+print("prbInputs is: ", prbInputs)
+
 destroyCommand='terraform destroy -auto-approve'
-
-pathToProjectRepoBuildCalls = "/home/aci-user/cloned-repos/agile-cloud-manager/calls-to-modules/azure-pipelines-project-repo-build-resources/"
-destroyProjectRepoBuildCommand=destroyCommand+getAzdoProviderInputs+getAzdoProjectRepoBuildAutoInputs+getAzdoProjectRepoBuildManualInputs+getAzurermProviderInputs
+destroyPrbCommand=destroyCommand+prbInputs
+pathToPrbCalls = acmRootDir+"calls-to-modules/azure-pipelines-project-repo-build-resources/"
+  
+################################################################################################ 
+### Step Three: Prepare the backend config for the azure-pipelines-project-repo-build-resources module
+##############################################################################################
+initPrbCommand = initCommand + prbBackendConfig
 
 ##############################################################################################
-### Step One: Check to make sure that the azure devops terraform provider has been installed
+### Step Five: Initialize the Terraform backend for the azure-pipelines-project-repo-build-resources module
 ##############################################################################################
-
-#NEED TO ADD THIS.
-
-##############################################################################################
-### Step Two: Apply The azure-pipelines-project-repo-build module
-##############################################################################################
-
-depfunc.runTerraformCommand(initCommand, pathToProjectRepoBuildCalls)
-depfunc.runTerraformCommand(destroyProjectRepoBuildCommand, pathToProjectRepoBuildCalls)
+depfunc.runTerraformCommand(initPrbCommand, pathToPrbCalls)
+depfunc.runTerraformCommand(destroyPrbCommand, pathToPrbCalls)
 
 print("Back in destroyProjectRepoBuild.py .")
-
