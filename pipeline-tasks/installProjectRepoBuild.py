425615488aa965fc441ca41009f411618e69fa47
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-27 16:38:08 -0700

    Update installProjectRepoBuild.py

--- pipeline-tasks/installProjectRepoBuild.py
+++ pipeline-tasks/installProjectRepoBuild.py
@@ -1,5 +1,4 @@
 print("Inside installProjectRepoBuild.py script.")
-  
 import sys 
 import deploymentFunctions as depfunc
 import os 
@@ -8,27 +7,12 @@ import yaml
 ##############################################################################################
 ### Step One:  Install azure devops extension for az client
 ##############################################################################################
-import subprocess
-import re
+#import subprocess
+#import re
+#ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
-ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
-
-def runShellCommand(commandToRun):
-    print("Inside runShellCommand(..., ...) function. ")
-    print("commandToRun is: " +commandToRun)
-
-    proc = subprocess.Popen( commandToRun,cwd=None, stdout=subprocess.PIPE, shell=True)
-    while True:
-      line = proc.stdout.readline()
-      if line:
-        thetext=line.decode('utf-8').rstrip('\r|\n')
-        decodedline=ansi_escape.sub('', thetext)
-        print(decodedline)
-      else:
-        break
-  
 addExteensionCommand = 'az extension add --name azure-devops'
-runShellCommand(addExteensionCommand)
+depfunc.runShellCommand(addExteensionCommand)
 
 #////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -72,140 +56,13 @@ print("depfunc.subscription_name is: ", depfunc.subscription_name)
 ##############################################################################################
 ### Step Three: Prepare the input variables for the azure-pipelines-project-repo-build-resources module
 ##############################################################################################
-def getProjectRepoBuildBackendConfig(yamlInputFile, awsCredFile):
-  varsString = ''
-  awsPublicAccessKey = ''
-  awsSecretAccessKey = ''
-  with open(yamlInputFile) as f:
-    topLevel_dict = yaml.safe_load(f)
-    for item in topLevel_dict:
-      if re.match("awsBackendTF", item):
-        awsBackendItems = topLevel_dict.get(item)
-        for awsBackendItem in awsBackendItems: 
-          if re.match("awsPublicAccessKey", awsBackendItem):
-            print(awsBackendItem, " is: ", awsBackendItems.get(awsBackendItem))
-            awsPublicAccessKey = awsBackendItems.get(awsBackendItem)
-          if re.match("awsSecretAccessKey", awsBackendItem):
-            print(awsBackendItem, " is: ", awsBackendItems.get(awsBackendItem))
-            awsSecretAccessKey = awsBackendItems.get(awsBackendItem)
-          if re.match("s3BucketNameTF", awsBackendItem):
-            print(awsBackendItem, " is: ", awsBackendItems.get(awsBackendItem))
-            varsString = varsString + " -backend-config \"bucket=" + awsBackendItems.get(awsBackendItem) +"\""  
-          if re.match("s3BucketRegionTF", awsBackendItem):
-            print(awsBackendItem, " is: ", awsBackendItems.get(awsBackendItem))
-            varsString = varsString + " -backend-config \"region=" + awsBackendItems.get(awsBackendItem) +"\""  
-          if re.match("dynamoDbTableNameTF", awsBackendItem):
-            print(awsBackendItem, " is: ", awsBackendItems.get(awsBackendItem))
-            varsString = varsString + " -backend-config \"dynamodb_table=" + awsBackendItems.get(awsBackendItem) +"\""  
-          if re.match("moduleKeyProjectRepoBuild", awsBackendItem):
-            print(awsBackendItem, " is: ", awsBackendItems.get(awsBackendItem))
-            varsString = varsString + " -backend-config \"key=" + awsBackendItems.get(awsBackendItem) +"\""  
-  #REPLACE THE FOLLOWING BLOCK WITH MORE ADVANCED VERSION CAPABLE OF HANDLING MULTIPLE ACCOUNTS
-  if ((len(awsPublicAccessKey) > 3) and (len(awsSecretAccessKey) > 3)):  
-    with open(awsCredFile, "w") as file:
-      lineToAdd = '[default]\n'
-      file.write(lineToAdd)
-      lineToAdd = "aws_access_key_id="+awsPublicAccessKey+"\n"
-      file.write(lineToAdd)
-      lineToAdd = "aws_secret_access_key="+awsSecretAccessKey+"\n"
-      file.write(lineToAdd)
-  print("varsString is: ", varsString)
-  return varsString
 
-def getProjectsReposBuildInputs(yamlInputFile, awsCredFile, prbSecretsFile, subscriptionId, tenantId, pipeResourceGroupRegion, pipeResourceGroupName, pipeKeyVaultName, pipeSubnetId, subscriptionName):
-  print("inside getProjectsReposBuildInputs(...,...,...) function.")
-  awsPublicAccessKey = ''
-  awsSecretAccessKey = ''
-  projectName = ''
-  repoName = ''
-  buildName = ''
-  varsString = ''
-  azdoOrgPAT = ''
-  clientSecret = ''
-  with open(yamlInputFile) as f:
-    topLevel_dict = yaml.safe_load(f)
-    for item in topLevel_dict:
-      print("item is: ", item)
-      if re.match("azdoConnection", item):  
-        connectionItems = topLevel_dict.get(item)  
-        for connectionItem in connectionItems:
-          if re.match("azdoOrgServiceURL", connectionItem):
-            print(connectionItem, " is: ", connectionItems.get(connectionItem))
-            varsString = varsString + " -var=\""+ connectionItem + "=" + connectionItems.get(connectionItem) +"\""  
-          if re.match("clientName", connectionItem):
-            print(connectionItem, " is: ", connectionItems.get(connectionItem))
-            varsString = varsString + " -var=\""+ connectionItem + "=" + connectionItems.get(connectionItem) +"\""  
-          if re.match("serviceConnectionName", connectionItem):
-            print(connectionItem, " is: ", connectionItems.get(connectionItem))
-            varsString = varsString + " -var=\""+ connectionItem + "=" + connectionItems.get(connectionItem) +"\""  
-          if re.match("clientId", connectionItem):
-            print(connectionItem, " is: ", connectionItems.get(connectionItem))
-            varsString = varsString + " -var=\""+ connectionItem + "=" + connectionItems.get(connectionItem) +"\""  
-          if re.match("azdoOrgPAT", connectionItem):
-            azdoOrgPAT = connectionItems.get(connectionItem)
-          if re.match("clientSecret", connectionItem):
-            clientSecret = connectionItems.get(connectionItem)
-      if re.match("projectRepoBuild", item):
-        projectRepoBuild = topLevel_dict.get(item)
-        for prbItem in projectRepoBuild:
-            if re.match("sourceRepo", prbItem):
-              print(prbItem, " is: ", projectRepoBuild.get(prbItem))
-              varsString = varsString + " -var=\""+ prbItem + "=" + projectRepoBuild.get(prbItem) +"\""  
-              nameStr = projectRepoBuild.get(prbItem)
-              nameStr = nameStr.replace(" ", "")
-              if nameStr.endswith('.git'):
-                nameStr = nameStr[:-4]
-              nameStr = nameStr.rpartition('/')[2]
-              repoName = nameStr
-              buildName = repoName
-              projectName = repoName + "Project"
-            if re.match("storageAccountNameTerraformBackend", prbItem):
-              print(prbItem, " is: ", projectRepoBuild.get(prbItem))
-              varsString = varsString + " -var=\""+ prbItem + "=" + projectRepoBuild.get(prbItem) +"\""  
-            if re.match("storageContainerNameTerraformBackend", prbItem):
-              print(prbItem, " is: ", projectRepoBuild.get(prbItem))
-              varsString = varsString + " -var=\""+ prbItem + "=" + projectRepoBuild.get(prbItem) +"\""  
-            if re.match("awsPublicAccessKey", prbItem):
-              awsPublicAccessKey = projectRepoBuild.get(prbItem)
-            if re.match("awsSecretAccessKey", prbItem):
-              awsSecretAccessKey = projectRepoBuild.get(prbItem)
-  varsString = varsString + " -var=\"projectName=" + projectName +"\""  
-  varsString = varsString + " -var=\"repoName=" + repoName +"\""  
-  varsString = varsString + " -var=\"buildName=" + buildName +"\""  
-  varsString = varsString + " -var=\"subscriptionId=" + subscriptionId +"\""  
-  varsString = varsString + " -var=\"tenantId=" + tenantId +"\""  
-  varsString = varsString + " -var=\"pipeResourceGroupRegion=" + pipeResourceGroupRegion +"\""  
-  varsString = varsString + " -var=\"pipeResourceGroupName=" + pipeResourceGroupName +"\""  
-  varsString = varsString + " -var=\"pipeKeyVaultName=" + pipeKeyVaultName +"\""  
-  varsString = varsString + " -var=\"pipeSubnetId=" + pipeSubnetId +"\""  
-  varsString = varsString + " -var=\"subscriptionName=" + subscriptionName +"\""  
-  with open(prbSecretsFile, "w") as file:
-    lineToAdd = "azdoOrgPAT=\""+azdoOrgPAT +"\"\n"
-    file.write(lineToAdd)
-    lineToAdd = "clientSecret=\""+clientSecret +"\"\n"
-    file.write(lineToAdd)
-    varsString = varsString + " -var-file=\""+ prbSecretsFile +"\""
-  #REPLACE THE FOLLOWING WITH MORE ADVANCED VERSION CAPABLE OF HANDLING MULTIPLE ACCOUNTS.
-  if ((len(awsPublicAccessKey) > 3) and (len(awsSecretAccessKey) > 3)):  
-    with open(awsCredFile, "w") as file:
-      lineToAdd = '[default]\n'
-      file.write(lineToAdd)
-      lineToAdd = "aws_access_key_id="+awsPublicAccessKey+"\n"
-      file.write(lineToAdd)
-      lineToAdd = "aws_secret_access_key="+awsSecretAccessKey+"\n"
-      file.write(lineToAdd)
-  print("varsString is: ", varsString)
-  return varsString
-
-prbBackendConfig = getProjectRepoBuildBackendConfig(myYamlInputFile, awsCredFile)
-prbInputs = getProjectsReposBuildInputs(myYamlInputFile, awsCredFile, prbSecretsFile, depfunc.subscription_id, depfunc.tenant_id, depfunc.resourceGroupLocation, depfunc.resourceGroupName, depfunc.pipeKeyVaultName, depfunc.pipeSubnetId, depfunc.subscription_name )
-
-
+prbBackendConfig = depfunc.getProjectRepoBuildBackendConfig(myYamlInputFile, awsCredFile)
+prbInputs = depfunc.getProjectsReposBuildInputs(myYamlInputFile, awsCredFile, prbSecretsFile, depfunc.subscription_id, depfunc.tenant_id, depfunc.resourceGroupLocation, depfunc.resourceGroupName, depfunc.pipeKeyVaultName, depfunc.pipeSubnetId, depfunc.subscription_name )
 
 print("prbBackendConfig is: ", prbBackendConfig)
 print("prbInputs is: ", prbInputs)
 
-## /////////////////////////////////////////////////////////////////////////////
 applyCommand='terraform apply -auto-approve'
 ## prbVars = depfunc.getProjectRepoBuildInputs(myYamlInputFile, foundationSecretsFile, depfunc.subscription_id, depfunc.tenant_id, depfunc.resourceGroupLocation, depfunc.resourceGroupName, depfunc.pipeKeyVaultName, depfunc.pipeSubnetId, depfunc.azuredevops_subscription_name)
 applyPrbCommand=applyCommand+prbInputs
@@ -225,33 +82,3 @@ initPrbCommand = initCommand + prbBackendConfig
 depfunc.runTerraformCommand(initPrbCommand, pathToPrbCalls)
 depfunc.runTerraformCommand(applyPrbCommand, pathToPrbCalls)
 print("Back in installProjectRepoBuild.py .")
-
-## THE INPUT VARIABLES FOR EACH PRB ARE:
-# .awsPublicAccessKey
-# .awsSecretAccessKey
-# .s3BucketNameTF
-# .s3BucketRegionTF
-# .dynamoDbTableNameTF
-# .moduleKeyProjectRepoBuild
-
-# _/\subscriptionName
-# _/\subscriptionId
-# _/\tenantId
-# .clientName
-# .clientId
-# .clientSecret
-# .serviceConnectionName
-# .storageAccountNameTerraformBackend
-# .storageContainerNameTerraformBackend
-# _/\pipeSubnetId
-# _/\pipeResourceGroupRegion
-# _/\pipeResourceGroupName
-# _/\pipeKeyVaultName
-# .azdoOrgPAT
-# .azdoOrgServiceURL
-# .sourceRepo
-# .projectName
-# .repoName
-# .buildName
-# .awsPublicAccessKey
-# .awsSecretAccessKey
