77299d90aaa34ecd779ae1188206ea041b3f599a
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-08-28 13:30:48 -0700

    Update destroyFoundation.py

--- pipeline-tasks/destroyFoundation.py
+++ pipeline-tasks/destroyFoundation.py
@@ -5,11 +5,11 @@ import sys
 import deploymentFunctions as depfunc
 
 # #The following are created/populated when you run  setup.py
-myYamlInputFile = '/home/aci-user/staging/foundationConfig.yaml'
-foundationSecretsFile = '/home/aci-user/vars/agile-cloud-manager/foundation-secrets.tfvars'
+myYamlInputFile = '/home/agile-cloud/staging/foundationConfig.yaml'
+foundationSecretsFile = '/home/agile-cloud/vars/agile-cloud-manager/foundation-secrets.tfvars'
 
 #The awsCredFile is for the terraform backend that will store state for the azure infrastructure created for the agile cloud manager.
-awsCredFile = '/home/aci-user/.aws/credentials'
+awsCredFile = '/home/agile-cloud/.aws/credentials'
 
 initCommand='terraform init '
 
