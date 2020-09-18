0f7d47e1e5f2a2c79ed976c6c32c36fe5a3bbeac
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-18 15:22:29 -0700

    Update variables.tf

--- modules/azure-devops-project/variables.tf
+++ modules/azure-devops-project/variables.tf
@@ -8,7 +8,6 @@ variable "tenantId" { }
 variable "clientName" { }  
 variable "clientId" { }  
 variable "clientSecret" { }  
-#variable "serviceConnectionName" { } 
 variable "azdoOrgPAT" { }  
 variable "azdoOrgServiceURL" { }  
 variable "projectName" { }  
@@ -16,13 +15,3 @@ variable "projectName" { }
 #Use this data source to access the configuration of the azurerm provider, which you configured using the above variables:
 data "azurerm_client_config" "current" {}
 
-##Define the output variables
-output "azuredevops_project_id" { value = azuredevops_project.project.id }
-output "azuredevops_project_name" { value = azuredevops_project.project.project_name }
-output "azuredevops_organization_service_url" { value = var.azdoOrgServiceURL }
-output "azuredevops_subscription_name" { value = var.subscriptionName }  
-output "azuredevops_subscription_id" { value = var.subscriptionId }  
-output "azuredevops_client_name" { value = var.clientName }  
-#output "azuredevops_service_connection_name" { value = var.serviceConnectionName }  
-
-output "azuredevops_service_connection_id" { value = azuredevops_serviceendpoint_azurerm.endpointazure.id }
