7591ebfe53ebbf45a3bf7d58a03ee5529a0462b7
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-18 15:03:37 -0700

    Update providers.tf

--- modules/azure-devops-repo-build/providers.tf
+++ modules/azure-devops-repo-build/providers.tf
@@ -1,18 +1,18 @@
 
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
+# # Configure the Microsoft Azure Provider
+# provider "azurerm" {
+#     # The "feature" block is required for AzureRM provider 2.x. 
+#     # If you're using version 1.x, the "features" block is not allowed.
+#     version = "~>2.0"
 
-    subscription_id = var.subscriptionId
-    client_id       = var.clientId
-    client_secret   = var.clientSecret
-    tenant_id       = var.tenantId
+#     subscription_id = var.subscriptionId
+#     client_id       = var.clientId
+#     client_secret   = var.clientSecret
+#     tenant_id       = var.tenantId
 
-    #The following features block will prevent Azure from retaining the key vault each time you run terraform destroy, provided purge is set to "true".  If remove the key_vault feature, make sure to retain an empty features {} block.
-    features { }
-}
+#     #The following features block will prevent Azure from retaining the key vault each time you run terraform destroy, provided purge is set to "true".  If remove the key_vault feature, make sure to retain an empty features {} block.
+#     features { }
+# }
 
 provider "azuredevops" {
   version = ">= 0.0.1"
