15b52ef45a6e7d10526d577a939e95e916636e6c
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-18 15:01:27 -0700

    Update repo-build.tf

--- modules/azure-devops-repo-build/repo-build.tf
+++ modules/azure-devops-repo-build/repo-build.tf
@@ -1,19 +1,19 @@
 #the azure devops repository and build
 
 resource "azuredevops_git_repository" "repository" {
-  project_id = azuredevops_project.project.id
+  project_id = data.azuredevops_project.p.id
   name = var.repoName
   initialization {
     init_type = "Uninitialized"
   }
   provisioner "local-exec" {
-    command = "az repos import create --git-source-url \"${var.sourceRepo}\" --repository \"${azuredevops_git_repository.repository.name}\" --organization \"${var.azdoOrgServiceURL}\" --project \"${azuredevops_project.project.project_name}\""
+    command = "az repos import create --git-source-url \"${var.sourceRepo}\" --repository \"${azuredevops_git_repository.repository.name}\" --organization \"${var.azdoOrgServiceURL}\" --project \"${data.azuredevops_project.p.name}\""
     working_dir = "/home/agile-cloud/cloned-repos/agile-cloud-manager/pipeline-tasks/"
   }
 }
 
 resource "azuredevops_build_definition" "build" {
-  project_id = azuredevops_project.project.id
+  project_id = data.azuredevops_project.p.id
   name       = var.buildName
   ci_trigger {
     use_yaml = true
