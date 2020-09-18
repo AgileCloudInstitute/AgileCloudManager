7373210437c27791976ae42f6c30e0398ed378d3
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-18 13:03:09 -0700

    Update project-repo-build.tf

--- modules/azure-pipelines-project-repo-build-resources/project-repo-build.tf
+++ modules/azure-pipelines-project-repo-build-resources/project-repo-build.tf
@@ -1,36 +1,10 @@
-#The Azure DevOps resources, including a project, a repository, and a build.
+#The Azure DevOps project.
 
 resource "azuredevops_project" "project" {
   project_name       = var.projectName
   visibility         = "private"
   version_control    = "Git"
   work_item_template = "Agile"
-}
-
-resource "azuredevops_git_repository" "repository" {
-  project_id = azuredevops_project.project.id
-  name = var.repoName
-  initialization {
-    init_type = "Uninitialized"
-  }
-  provisioner "local-exec" {
-    command = "az repos import create --git-source-url \"${var.sourceRepo}\" --repository \"${azuredevops_git_repository.repository.name}\" --organization \"${var.azdoOrgServiceURL}\" --project \"${azuredevops_project.project.project_name}\""
-    working_dir = "/home/agile-cloud/cloned-repos/agile-cloud-manager/pipeline-tasks/"
-  }
-}
-
-resource "azuredevops_build_definition" "build" {
-  project_id = azuredevops_project.project.id
-  name       = var.buildName
-  ci_trigger {
-    use_yaml = true
-  }
-
-  repository {
-    repo_type   = "TfsGit"
-    repo_id     = azuredevops_git_repository.repository.id
-    yml_path    = "azure-pipelines.yml"
-  }
 }
 
 resource "azuredevops_serviceendpoint_azurerm" "endpointazure" {  
