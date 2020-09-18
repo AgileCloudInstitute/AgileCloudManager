ded128ef9f87da3d18e0ee1aacbd663ee6e207aa
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-18 13:54:51 -0700

    Update variables.tf

--- modules/azure-devops-project/variables.tf
+++ modules/azure-devops-project/variables.tf
@@ -1,8 +1,6 @@
 ####################################################################################################################
 ####Define the input variables:  
 ####################################################################################################################
-
-###The following are for 2nd iteration
 variable "subscriptionName" { }  
 variable "subscriptionId" { }  
 variable "tenantId" { }  
@@ -10,38 +8,21 @@ variable "tenantId" { }
 variable "clientName" { }  
 variable "clientId" { }  
 variable "clientSecret" { }  
-variable "serviceConnectionName" { } 
-variable "subnetId" { }  
-variable "resourceGroupLocation" { }  
-variable "resourceGroupName" { }  
+#variable "serviceConnectionName" { } 
 variable "azdoOrgPAT" { }  
 variable "azdoOrgServiceURL" { }  
-variable "sourceRepo" { }  
 variable "projectName" { }  
-variable "repoName" { }  
-variable "buildName" { }  
 
-#Use this data source to access the configuration of the azurerm provider, which you configured above:
+#Use this data source to access the configuration of the azurerm provider, which you configured using the above variables:
 data "azurerm_client_config" "current" {}
 
 ##Define the output variables
-#Output the project id for use in script that will wrap this program.
 output "azuredevops_project_id" { value = azuredevops_project.project.id }
-output "azuredevops_git_repository_id" { value = azuredevops_git_repository.repository.id }
-output "azuredevops_git_repository_name" { value = azuredevops_git_repository.repository.name }
-output "azuredevops_git_repository_default_branch" { value = azuredevops_git_repository.repository.default_branch }
-output "azuredevops_git_repository_is_fork" { value = azuredevops_git_repository.repository.is_fork }
-output "azuredevops_git_repository_remote_url" { value = azuredevops_git_repository.repository.remote_url }
-output "azuredevops_git_repository_size" { value = azuredevops_git_repository.repository.size }
-output "azuredevops_git_repository_ssh_url" { value = azuredevops_git_repository.repository.ssh_url }
-output "azuredevops_git_repository_url" { value = azuredevops_git_repository.repository.url }
-output "azuredevops_git_repository_web_url" { value = azuredevops_git_repository.repository.web_url }
 output "azuredevops_project_name" { value = azuredevops_project.project.project_name }
-output "azuredevops_build_definition_id" { value = azuredevops_build_definition.build.id }
 output "azuredevops_organization_service_url" { value = var.azdoOrgServiceURL }
 output "azuredevops_subscription_name" { value = var.subscriptionName }  
 output "azuredevops_subscription_id" { value = var.subscriptionId }  
 output "azuredevops_client_name" { value = var.clientName }  
-output "azuredevops_service_connection_name" { value = var.serviceConnectionName }  
+#output "azuredevops_service_connection_name" { value = var.serviceConnectionName }  
 
 output "azuredevops_service_connection_id" { value = azuredevops_serviceendpoint_azurerm.endpointazure.id }
