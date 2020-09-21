eec6198f34aaab0ca2f18c767069805b795a16a0
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-21 11:20:45 -0700

    Update installProjectRepoBuild.py

--- pipeline-tasks/installProjectRepoBuild.py
+++ pipeline-tasks/installProjectRepoBuild.py
@@ -14,7 +14,40 @@ def createBackendConfigFileTerraform( dir_to_use ):
   f.write("  }\n")
   f.write("}\n")
   f.close()
-  
+
+import re
+def getProjectName(yamlInputFile):
+  print("inside getProjectName(...) function.")
+  projectName = ''
+  with open(yamlInputFile) as f:
+    topLevel_dict = yaml.safe_load(f)
+    for item in topLevel_dict:
+      print("item is: ", item)
+      if re.match("projectRepoBuild", item):
+        projectRepoBuild = topLevel_dict.get(item)
+        for prbItem in projectRepoBuild:
+            if re.match("projectName", prbItem):
+              print(prbItem, " is: ", projectRepoBuild.get(prbItem))
+              projectName = projectRepoBuild.get(prbItem)
+  print("projectName is: ", projectName)
+  return projectName
+
+import shutil
+
+def deleteContentsOfDirectoryRecursively(call_to_project_dir):  
+    if [f for f in os.listdir(call_to_project_dir) if not f.startswith('.')] == []:  
+        print(call_to_project_dir, " is empty. ")  
+    else: 
+        print(call_to_project_dir, " is NOT empty.  Deleting contents now, but consider backup strategy in case you do not want this auto-delete in your processes.  ")  
+        for the_file in os.listdir(call_to_project_dir):
+            file_path = os.path.join(call_to_project_dir, the_file)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            except Exception as e:
+                print(e)
+
 ##############################################################################################
 ### Step One:  Install azure devops extension for az client
 ##############################################################################################
@@ -65,23 +98,7 @@ print("depfunc.subscription_name is: ", depfunc.subscription_name)
 ##############################################################################################
 ### Step Three: Get Project Name from YAML
 ##############################################################################################
-import re
-def getProjectName(yamlInputFile):
-  print("inside getProjectName(...) function.")
-  projectName = ''
-  with open(yamlInputFile) as f:
-    topLevel_dict = yaml.safe_load(f)
-    for item in topLevel_dict:
-      print("item is: ", item)
-      if re.match("projectRepoBuild", item):
-        projectRepoBuild = topLevel_dict.get(item)
-        for prbItem in projectRepoBuild:
-            if re.match("projectName", prbItem):
-              print(prbItem, " is: ", projectRepoBuild.get(prbItem))
-              projectName = projectRepoBuild.get(prbItem)
-  print("projectName is: ", projectName)
-  return projectName
-  
+
 project_name = getProjectName(myYamlInputFile)
 #Create new directory for call to this specific project
 call_name = "call-to-" + project_name  
@@ -89,6 +106,10 @@ call_to_project_dir = acmRootDir+"calls-to-modules/instances/"+call_name
 print("call_to_project_dir is: ", call_to_project_dir)
 if not os.path.exists(call_to_project_dir):
     os.makedirs(call_to_project_dir)
+#Check whether directory is empty or not.  If it is NOT empty, then delete the contents  
+deleteContentsOfDirectoryRecursively(call_to_project_dir)
+print("Now confirming that directory is empty before moving on.  ")
+deleteContentsOfDirectoryRecursively(call_to_project_dir)
 
 # ##############################################################################################
 # ### Step Three: Prepare the input variables for the azure-pipelines-project-repo-build-resources module
