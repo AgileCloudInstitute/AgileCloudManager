7b8822fcb7289a14367462998960620c4a65bf6b
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-24 11:26:28 -0700

    Update variables.tf

--- modules/azure-devops-repo-build/variables.tf
+++ modules/azure-devops-repo-build/variables.tf
@@ -15,5 +15,8 @@ variable "buildName" { }
 
 data "azuredevops_project" "p" {  project_name = var.projectName  }
 
+output "azuredevops_build_definition_id" { value = azuredevops_build_definition.build.id }
+output "azuredevops_git_repository_name" { value = azuredevops_git_repository.repository.name }
+
 #Use this data source to access the configuration of the azurerm provider, which you configured with the input variables above:
 #data "azurerm_client_config" "current" {}
