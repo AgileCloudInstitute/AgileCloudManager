c77f38aad6b198a78b01ba955030181cdcf9ed4a
AgileCloudInstitute <Agile.Cloud.Institute@GreenRiverIT.com>
2022-08-24 12:02:47 -0700

    more upgrades

--- unitTests/test_controller_terraform_and_packer_two.py
+++ unitTests/test_controller_terraform_and_packer_two.py
@@ -12,7 +12,7 @@ import time
     ## First, navigating the terminal to the folder that is the PARENT of the folder that contains the agile-cloud-manager code.
     ## Second, make sure the directory structure is correct, including sibling directories for required repos, etc. 
     ## Third, running the following command in the terminal:
-       #python -m unittest AgileCloudManager.unitTests.test_controller_terraform_and_packer
+       #python -m unittest AgileCloudManager.unitTests.test_controller_terraform_and_packer_two
   
 class TestControllerPacker(unittest.TestCase):
   
@@ -652,45 +652,6 @@ class TestControllerPacker(unittest.TestCase):
     #THE FOLLOWING BLOCK DELETES THE KEY FILE, BUT YOU NEED THE KEYFILE DURING TEST DEVELOPMENT.
     self.deleteAcmKeys()
 
-##UNCOMMENT THIS NEXT FUNCTION BECAUSE IT WORKS FINE.  JUST COMMENTING IT HERE SO WE CAN ISOLATE OTHER TESTS BELOW IT DURING DEVELOPMENT.
-#  def test_terraform_foundation_VarsFragmentContents(self):
-#    self.addAcmDirToPath()
-#    from AgileCloudManager.app.command_builder import command_builder
-#    import AgileCloudManager.app.config_cliprocessor
-#    self.setAcmVariables(AgileCloudManager.app.config_cliprocessor)
-#    correctExpectedResponse = [
-#'subscriptionId="example-subscriptionId"',
-#'tenantId="example-tenantId"',
-#'clientId="example-clientId"',
-#'clientSecret="example-clientSecret"',
-#'resourceGroupRegion="eastus"',
-#'resourceGroupName="myEmptyTestRG"',
-#'vpcCIDR="10.0.0.0/16"',
-#'secondString="bencher"',
-#'clientName="example-clientName"',
-#'subName="example-subscriptionName"',
-#'labrador="isadog"',
-#'tName="isabird"',
-#'networkName="name-of-vnet"',
-#'owner="name-of-owner"',
-#'makePath="userCallingDir\\azure-building-blocks\\arm"',
-#'now="20220624125048860330"',
-#'addOrgTest="somestringtstaxy"'
-#    ]
-#    cb = command_builder()
-#    systemConfig = self.getSystemConfig_TerraformPacker()
-#    serviceType = None
-#    instance = systemConfig.get("foundation")
-#    mappedVariables = systemConfig.get('foundation').get('mappedVariables')
-#    tool = 'terraform'
-#    outputDict = {}
-#    print("xxxzzzvvv config_cliprocessor.inputVars.get('dirOfOutput') is: ", AgileCloudManager.app.config_cliprocessor.inputVars.get('dirOfOutput'))
-##    quit('ddddddd')
-#    returnVal = self.checkVarsReturnedAgainstExpected(cb, systemConfig, serviceType, instance, mappedVariables, tool, outputDict, correctExpectedResponse)
-#    #THE FOLLOWING BLOCK DELETES THE KEY FILE, BUT YOU NEED THE KEYFILE DURING TEST DEVELOPMENT.
-#    self.deleteAcmKeys()
-#    print('test_terraform_foundation_VarsFragmentContents() returnVal is: ', returnVal)
-#    self.assertTrue(returnVal)
 
 #UNCOMMENT THIS NEXT FUNCTION BECAUSE IT WORKS FINE.  JUST COMMENTING IT HERE SO WE CAN ISOLATE OTHER TESTS BELOW IT DURING DEVELOPMENT.
   def test_packer_image_VarsFragmentContents(self):
